2557ec553eb5bc808a67c922d1e6fdcbb541cb2a
dreaminbytes <40922667+dreaminbytes@users.noreply.github.com>
2023-08-18 23:11:39 +0100

    Initial commit

---
+++ EF6/NETFramework/DatabaseFirstSample/DatabaseFirstSample.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using DatabaseFirstSample.Data;
+
+namespace DatabaseFirstSample.ConsoleApp
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.Write("Enter a name for a new blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ EF6/NETFramework/DatabaseFirstSample/DatabaseFirstSample.WinFormsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+// Ref: https://learn.microsoft.com/en-us/ef/ef6/fundamentals/databinding/winforms
+
+namespace DatabaseFirstSample.WinFormsApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
